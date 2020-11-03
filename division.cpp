6ddf7da5a0e1fe8c65b1cc5f7a451a0a131a2755
DeLaFuente <66786190+DeLaFuente1999@users.noreply.github.com>
2020-11-03 20:19:53 +0100

    Add files via upload

---
+++ division.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n1,n2, division=0;
+	cout<<"Escriba un numero: "; cin>>n1;
+	cout<<"Escriba un numero: "; cin>>n2;
+	
+	division = n1 / n2;
+	
+	cout<<"La division es: "<<division<<endl;
+	
+	return 0;
+}
