b86f44a82cfe852eb06b92324c750fcf4ff8dd3e
DeLaFuente <66786190+DeLaFuente1999@users.noreply.github.com>
2020-11-10 18:22:16 +0100

    Delete division.php

--- division.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "Introduzca un numero";
-$numero1 = stream_get_line(STDIN, 1024, PHP_EOL);
-echo "Introduzca otro numero";
-$numero2 = stream_get_line(STDIN, 1024, PHP_EOL);
-echo "La division es: " . $numero1 . "/" . $numero2 . "=" . ($numero1/$numero2);
-?>
